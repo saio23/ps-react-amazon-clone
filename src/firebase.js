3ea2c84392add9a8fe571ad79bbee6c33348071f
Pantelis Sioukas <pantelis@irestech.net>
2020-09-25 08:20:14 +0300

    latest update

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,6 @@
 import firebase from "firebase";
+import firebaseConfig from './firebaseConfig'
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBM3mHiy5-sDPUxXXfana9ArbSIIkl8chw",
-    authDomain: "amazfclone-a7c3d.firebaseapp.com",
-    databaseURL: "https://amazfclone-a7c3d.firebaseio.com",
-    projectId: "amazfclone-a7c3d",
-    storageBucket: "amazfclone-a7c3d.appspot.com",
-    messagingSenderId: "507859081723",
-    appId: "1:507859081723:web:a3c82c11374802b750535e"
-  };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
